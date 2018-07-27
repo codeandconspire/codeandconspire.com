b336a91166b79178b1c0b0a78fffe256f31d5ab2
Anton Trollbäck <anton@trollback.se>
2018-07-27 15:31:58 +0200

    Add jalla setup

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = (ctx) => ({
-  plugins: [
-    require('postcss-import')(),
-    require('postcss-flexbugs-fixes')(),
-    require('autoprefixer')({browsers: [ 'last 2 versions', 'ie >= 9', 'Firefox ESR' ]})
-  ]
-})
