27a2610f2167a8df7ac70998b06f7851fa916a48
Anton Trollbäck <anton@trollback.se>
2018-10-10 17:16:09 +0200

    Add images

--- components/poster/index.js
+++ components/poster/index.js
@@ -1,10 +1,10 @@
 var html = require('choo/html')
 
 module.exports = Poster
-// <img class="Poster-bg" src="https://res.cloudinary.com/demo/image/fetch/c_scale,h_1342,q_auto/https://www.codeandconspire.com/${props.img.src}" alt="${props.img.alt}">
+
 function Poster (props) {
   return html`
-    <section class="Poster Poster--${props.id} ${props.light ? 'Poster--light' : ''}" id="poster-${props.id}">
+    <section class="Poster Poster--${props.id}" id="poster-${props.id}">
       <div class="Poster-body">
         <strong class="Poster-label">${props.label}</strong>
         <h1 class="Display Display--keep">${props.title}</h1>
@@ -12,7 +12,10 @@ function Poster (props) {
         <a class="Poster-link" target="_blank" rel="noopener noreferrer" href="${props.link.href}">${props.link.text}</a>
       </div>
       ${props.img ? html`
-        <img class="Poster-bg" src="/${props.img.src}" alt="${props.img.alt}">
+        <img class="Poster-bg" src="https://res.cloudinary.com/demo/image/fetch/c_scale,h_1342,q_auto/https://www.codeandconspire.com/${props.img.src}" alt="${props.img.alt}">
+      ` : null} 
+      ${props.embed ? html`
+        ${props.embed}
       ` : null} 
     </section>
   `
